65afd50de7538ac2e10457f4a0f8751967ed38a1
Noel Benzinger <noelbenzinger@gmail.com>
2019-12-20 20:24:27 -0600

    Fixed pointer bug when initializing library.

--- freetype.lisp
+++ freetype.lisp
@@ -3,24 +3,42 @@
 (defvar *library*)
 (export '*library*)
 
+(define-condition freetype-error (error)
+  ((function-name :initarg :function-name :reader function-name)
+   (error-code :initarg :error-code :reader error-code)
+   (error-message :initarg :error-message :reader error-message)))
+(export 'freetype-error)
+(export 'function-name)
+(export 'error-code)
+(export 'error-message)
+
+(defun error-string (error-code)
+  (c-fun freetype-ffi:ft-error-string error-code))
+(export 'error-string)
+
+(defmacro %handle-error-c-fun (fn &rest args)
+  (with-gensyms (error-code)
+    `(let ((,error-code (c-fun ,fn ,@args)))
+       (when (/= ,error-code 0)
+         (error 'freetype-error
+                :function-name ,(symbol-name fn)
+                :error-code ,error-code
+                :error-message (error-string ,error-code))))))
+
 (defun make-library ()
-  (cffi:with-foreign-objects ((library-ptr-ptr :pointer) (library-ptr :pointer))
-    (setf (cffi:mem-aref library-ptr-ptr :pointer) library-ptr)
-    (let* ((error-code (c-fun freetype-ffi:ft-init-free-type library-ptr-ptr))
-           (library-cffi-ptr (cffi:make-pointer (cffi:pointer-address library-ptr))))
-      (if (/= error-code 0)
-          ;; TODO: Generic error handler that throws a condition.
-          (format t "There was an error making the library!~%")
-          (autowrap:wrap-pointer library-cffi-ptr 'freetype-ffi:ft-library)))))
+  (cffi:with-foreign-objects ((library-ptr :pointer))
+    (%handle-error-c-fun freetype-ffi:ft-init-free-type library-ptr)
+    (autowrap:wrap-pointer (cffi:mem-ref library-ptr :pointer) 'freetype-ffi:ft-library)))
 (export 'make-library)
 
 (defun destroy-library (library)
-  (c-fun freetype-ffi:ft-done-free-type library))
+  (%handle-error-c-fun freetype-ffi:ft-done-free-type library))
 (export 'destroy-library)
 
 (defmacro with-init (&body body)
   `(let ((*library* (make-library)))
      (unwind-protect (progn ,@body)
+       (format t "Library: ~A~%" *library*)
        (destroy-library *library*))))
 (export 'with-init)
 
