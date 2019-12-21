1264fb89a7142b48d68207ac4fba62dd69a78424
Noel Benzinger <noelbenzinger@gmail.com>
2019-12-21 03:08:02 -0600

    Set font size.

--- freetype.lisp
+++ freetype.lisp
@@ -34,21 +34,34 @@
                 :error-code ,error-code
                 :error-message (error-name ,error-code))))))
 
-(defmacro def-record-method (wrapper-class method-name &rest record-fields)
-  "Generates a generic method named `method-name' that reads the record of `wrapper-class' described by `record-fields'"
+(defmacro def-record-method (wrapper-class method-name export-p &rest record-fields)
+  "Generates a generic method named `method-name' that reads the record of
+`wrapper-class' described by `record-fields'"
   (with-gensyms (wrapper)
     `(progn
       (defgeneric ,method-name (object))
       (defmethod ,method-name ((,wrapper ,wrapper-class))
-        (c-ref ,wrapper ,wrapper-class ,@record-fields)))))
+        (c-ref ,wrapper ,wrapper-class ,@record-fields))
+      ,@(when export-p `((export (quote ,method-name)))))))
 
 (defmacro def-record-methods (wrapper-class record-descriptors)
-  "Generates a generic method for each descriptor in `record-descriptors' which reads a record from `wrapper-class'.
+  "Generates a generic method for each descriptor in `record-descriptors' which
+reads a record from `wrapper-class'.
 
-Each record descriptor is a list whose first element is the generic method's name and the rest of the elements are the record fields"
+Each record descriptor is a list whose first element is the generic method's name,
+second element is a boolean indicating if the method should be exported, and the
+rest of the elements are the record fields"
   `(progn
      ,@(loop for descriptor in record-descriptors
             collect `(def-record-method ,wrapper-class ,@descriptor))))
+
+(defun from-26-6 (fixed-26-6)
+  (/ fixed-26-6 64))
+(export 'from-26-6)
+
+(defun to-26-6 (decimal)
+  (round (* decimal 64)))
+(export 'to-26-6)
 
 (defun make-library ()
   (cffi:with-foreign-objects ((library-ptr :pointer))
@@ -81,34 +94,52 @@ Each record descriptor is a list whose first element is the generic method's nam
 (export 'destroy-face)
 
 (def-record-methods freetype-ffi:ft-face-rec
-    ((num-faces :num-faces)
-     (face-index :face-index)
-     (face-flags :face-flags)
-     (style-flags :style-flags)
-     (num-glyphs :num-glyphs)
-     (family-name :family-name)
-     (style-name :style-name)
-     (num-fixed-sizes :num-fixed-sizes)
-     (available-sizes :available-sizes)
-     (num-charmaps :num-charmaps)
-     (charmaps :charmaps)
-     (bbox :bbox)
-     (units-per-em :units-per-em)
-     (ascender :ascender)
-     (descender :descender)
-     (height :height)
-     (max-advance-width :max-advance-width)
-     (max-advance-height :max-advance-height)
-     (underline-position :underline-position)
-     (underline-thickness :underline-thickness)
-     (glyph :glyph)
-     (size :size)
-     (charmap :charmap)))
+    ((num-faces t :num-faces)
+     (face-index t :face-index)
+     (face-flags t :face-flags)
+     (style-flags t :style-flags)
+     (num-glyphs t :num-glyphs)
+     (family-name t :family-name)
+     (style-name t :style-name)
+     (num-fixed-sizes t :num-fixed-sizes)
+     (available-sizes t :available-sizes)
+     (num-charmaps t :num-charmaps)
+     (charmaps t :charmaps)
+     (bbox t :bbox)
+     (units-per-em t :units-per-em)
+     (ascender t :ascender)
+     (descender t :descender)
+     (height t :height)
+     (max-advance-width t :max-advance-width)
+     (max-advance-height t :max-advance-height)
+     (underline-position t :underline-position)
+     (underline-thickness t :underline-thickness)
+     (glyph t :glyph)
+     (size t :size)
+     (charmap t :charmap)))
+
+(defgeneric set-char-size (face &key char-width char-height
+                           horizontal-resolution vertical-resolution))
+(export 'set-char-size)
+
+(defmethod set-char-size ((face freetype-ffi:ft-face-rec) &key (char-width 0) (char-height 0)
+                          (horizontal-resolution 0) (vertical-resolution 0))
+  (handle-error-c-fun freetype-ffi:ft-set-char-size face char-width char-height
+                      horizontal-resolution vertical-resolution)
+  face)
+
+(defgeneric set-pixel-size (face &key pixel-width pixel-height ))
+(export 'set-pixel-size)
+
+(defmethod set-pixel-size ((face freetype-ffi:ft-face-rec) &key (pixel-width 0) (pixel-height 0))
+  (handle-error-c-fun freetype-ffi:ft-set-pixel-sizes face pixel-width pixel-height)
+  face)
 
 #+nil
 (freetype:with-init
   (format t "Library loaded!~%")
   (let ((face (make-face "projects/freetype/BebasNeueBold.ttf")))
+    (set-char-size face :char-width (to-26-6 12) :horizontal-resolution 8 :vertical-resolution 12)
     (format t "Face: ~A~%" face)
     (format t "Number of faces: ~A~%" (num-faces face))
     (format t "Ascender: ~A~%" (ascender face))
